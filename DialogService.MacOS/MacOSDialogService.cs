63d196c5050801a48288729b0fafe75b3b605e61
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-02 15:02:03 +0300

    Add project files.

---
+++ DialogService.MacOS/MacOSDialogService.cs
@@ -0,0 +1,16 @@
+﻿using DialogService.Items;
+using System;
+
+namespace DialogService.MacOS
+{
+    /// <summary>
+    /// Dialog service implementation for macOS
+    /// </summary>
+    public class MacOSDialogService : IDialogService
+    {
+        public void Show(Dialog dialog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DialogService.MacOS/Platform.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService
+{
+    /// <summary>
+    /// Functionality to get macOS platform implementation of dialog service
+    /// </summary>
+    public class PlatformImplementation : AbstractPlatform
+    {
+        /// <summary>
+        /// Gets MacOS dialog service implementation
+        /// </summary>
+        /// <returns></returns>
+        public override IDialogService Get() => new MacOS.MacOSDialogService();
+    }
+}
